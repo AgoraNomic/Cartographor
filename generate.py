4c8b461e9a75fe0843b9818b5c90ac13d25e2886
Reuben Staley <lighthousemaniac@gmail.com>
2017-11-25 08:28:23 -0700

    Add an extensible metadata system

--- generate.py
+++ generate.py
@@ -9,11 +9,23 @@ table_data = json.loads(get_contents("data/map.json"))
 
 table_html = ""
 
-for i, o in enumerate(table_data):
-    table_html += "    <tr class='row' id='r%d'>\n" % (i)
-    for j, p in enumerate(o):
-        table_html += "      <td class='cell' id='r%dc%d' type='%s' onmouseover='getStats(%d, %d)'></td>\n" % (i, j, p, j, i)
-    table_html += "    </tr>\n"
+for i in range(0, 9):
+    table_html += "    <tr class='row' id='r%d'>\n      " % (i)
+    for j in range(0, 9):
+        table_html += "<td class='cell' id='r%dc%d' onmouseover='getStats(%d, %d)'\n      ></td>" % (i, j, j, i)
+    table_html += "</tr>\n"
+
+table_list = table_html.split("\n")
+cell_list = [i-1 for i, j in enumerate(table_list) if j.find("</td>") != -1]
+
+print(cell_list)
+
+for attr, i in table_data.items():
+    for row, j in enumerate(i):
+        for col, k in enumerate(j):
+            table_list[cell_list[row*9+col]] += " %s='%s'" % (attr, k)
+
+table_html = "\n".join(table_list)
 
 html = html.replace("{{tabledata}}", table_html)
 
--- stats.js
+++ stats.js
@@ -1,9 +1,9 @@
-var stats = ["type"];
+var stats = ["type", "ownership"];
 
 function getStats(x, y) {
 	s = document.getElementById("stats");
-	s.innerHTML = "";
+	s.innerHTML = "lat: " + y + "<br>long: " + x + "<br>";
 	for (var i = 0; i < stats.length; i++) {
-		s.innerHTML += stats[i] + ": " + document.getElementById("r" + y + "c" + x).getAttribute(stats[i]);
+		s.innerHTML += stats[i] + ": " + document.getElementById("r" + y + "c" + x).getAttribute(stats[i]) + "<br>";
 	}
 }
