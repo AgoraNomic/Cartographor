d702380f4d85dddb005d8796814e55a9b5db69c6
Reuben Staley <lighthousemaniac@gmail.com>
2018-02-10 09:58:16 -0700

    integrate defaults into changes for better chronology

--- generate.py
+++ generate.py
@@ -11,7 +11,6 @@ def get_contents(fn):
 
 html = get_contents("template.html") # get the template file
 changes = [i for i in json.loads(get_contents("data/changes.json")) if type(i) != type("")] # get all recorded changes
-defaults = json.loads(get_contents("data/defaults.json")) # get the stats to track and their default values
 
 table_html = "" # this contains html code that will be injected into the file
 table_data = [] # this will contain dictionaries that describe each square
@@ -22,21 +21,36 @@ for row in range(-9, 10):
     table_data.append([])
     for col in range(-9, 10):
         table_html += "<td class='cell' id='r%dc%d' onmouseover='getStats(%d, %d)'\n      ></td>" % (row, col, col, row)
-        table_data[row+9].append(defaults.copy())
+        table_data[row+9].append({})
     table_html += "</tr>\n"
+
+to_track = [] # this will store all the values we want to track, as set by the "track" command.
+defaults = {} # this will hold the default values of the values in to_track.
 
 # uses data/changes.json to update the map to its current state
 for change in changes:
-    row = change["coords"][0]+9
-    col = change["coords"][1]+9
-    for attr, value in change.items():
-        if attr != "coords":
-            table_data[row][col][attr] = value
+    command = change[0]
+    args = change[1]
+    if command == "track":
+        to_track.append(args["name"])
+        defaults[args["name"]] = args["default"]
+    
+    elif command == "set":
+        row = args["coords"][0]+9
+        col = args["coords"][1]+9
+        for attr, value in args.items():
+            if attr != "coords":
+                table_data[row][col][attr] = value
+
+# add values that are still default
+for row in table_data:
+    for col in row:
+        for attr in to_track:
+            try: col[attr]
+            except KeyError: col[attr] = defaults[attr]
 
 table_list = table_html.split("\n") # get a list of lines to make searching easier
 cell_list = [i-1 for i, j in enumerate(table_list) if j.find("</td>") != -1] # get the numbers of the lines we want to add to
-
-# print(len(table_list))
 
 # generate html for table 
 for row, i in enumerate(table_data):
