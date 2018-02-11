ad95d4b93e7f29b07547d6b0205a8dd8980c3e78
Reuben Staley <lighthousemaniac@gmail.com>
2018-02-11 11:46:46 -0700

    map resizing

--- generate.py
+++ generate.py
@@ -1,5 +1,6 @@
 import json
 from datetime import datetime as dt
+from re import sub
 
 # get the date for the generated html file name
 date = dt.now().date()
@@ -9,25 +10,39 @@ def get_contents(fn):
     with open(fn) as f:
         return f.read()
 
-def create_abbreviations(long_names):
-    pass
-
 html = get_contents("template.html") # get the template file
 changes = [i for i in json.loads(get_contents("data/changes.json")) if type(i) != type("")] # get all recorded changes
 
 table_html = "" # this contains html code that will be injected into the file
 table_data = [] # this will contain dictionaries that describe each square
 
-# prepares the table_html and table_data variables with 19x19 grids filled with default values
-for row in range(-9, 10):
-    table_html += "    <tr class='row' id='r%d'>\n      " % row
-    table_data.append([])
-    for col in range(-9, 10):
-        table_html += "<td class='cell' id='r%dc%d' onmouseover='getStats(%d, %d)'\n      ></td>" % (row, col, col, row)
-        table_data[row+9].append({})
-    table_html += "</tr>\n"
-
 to_track = [] # this will store all the values we want to track, as set by the "track" command
+
+# these are the lowest possible latitude and longitude values.
+min_lat = 0
+min_lon = 0
+max_lat = 0
+max_lon = 0
+
+# generates single-letter strings from a list for labeling.
+def create_initials(long_names):
+    long_names = [str(i) for i in long_names]
+    initials = {}
+    for name in long_names:
+        name = sub(r"[\*\. ]", "", name)
+    long_names.sort(key=len)
+    for name in long_names:
+        index = 0
+        found = False
+        while found == False:
+            for i,initial in initials.items():
+                if name[index] == initial:
+                    index +=1
+                    break
+            else:
+                initials[name] = name[index]
+                found = True
+    return initials
 
 # uses data/changes.json to update the map to its current state
 for change in changes:
@@ -48,10 +63,38 @@ for change in changes:
                 except: pass
         
     elif command == "set":
-        row = args["coords"][0]+9
-        col = args["coords"][1]+9
+        row = args["coords"][0]-min_lat
+        col = args["coords"][1]-min_lon
         for attr, value in args.items():
             if attr != "coords": table_data[row][col][attr] = value
+        try:
+            if args["type"] == "a":
+                table_data[row][col] = {}
+        except: pass
+
+    elif command == "resize":
+        try: min_lat = args["min_lat"]
+        except: pass
+        try: min_lon = args["min_lon"]
+        except: pass
+        try: max_lat = args["max_lat"]
+        except: pass
+        try: max_lon = args["max_lon"]
+        except: pass
+        
+        max_row = max_lat - min_lat + 1
+        max_col = max_lon - min_lon + 1
+
+        print(max_row, max_col)
+
+        table_data = table_data[:max_row]
+        for row in range(max_row):
+            try: table_data[row]
+            except: table_data.append([])
+            table_data[row] = table_data[row][:max_col]
+            for col in range(max_col):
+                try: table_data[row][col]
+                except: table_data[row].append({})
 
 # add values that are still default
 for row in table_data:
@@ -61,6 +104,15 @@ for row in table_data:
             try: col[name]
             except KeyError: col[name] = attr["default"]
 
+# populates table_html
+for row, i in enumerate(table_data):
+    lat = row + min_lat
+    table_html += "    <tr class='row' id='r%d'>\n      " % lat
+    for col,j in enumerate(i):
+        lon = col + min_lon
+        table_html += "<td class='cell' id='r%dc%d' onmouseover='getStats(%d, %d)'\n      ></td>" % (lat, lon, lon, lat)
+    table_html += "</tr>\n"
+
 table_list = table_html.split("\n") # get a list of lines to make searching easier
 cell_list = [i-1 for i, j in enumerate(table_list) if j.find("</td>") != -1] # get the numbers of the lines we want to add to
 
@@ -68,7 +120,7 @@ cell_list = [i-1 for i, j in enumerate(table_list) if j.find("</td>") != -1] # g
 for row, i in enumerate(table_data):
     for col, j in enumerate(i):
         for attr, k in j.items():
-            table_list[cell_list[row*19+col]] += " %s='%s'" % (attr, k)
+            table_list[cell_list[row*(max_lon-min_lon+1)+col]] += " %s='%s'" % (attr, k)
 
 # add the generated table html to the template
 table_html = "\n".join(table_list)
